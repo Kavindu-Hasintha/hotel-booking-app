3f658d6495c89a1f726ea9e452a3867c7b33d5c9
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-12-24 13:39:41 +0530

    Add new room API is completed

--- backend/src/main/java/org/code/backend/repository/RoomRepository.java
+++ backend/src/main/java/org/code/backend/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package org.code.backend.repository;
 
-public interface RoomRepository {
+import org.code.backend.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
--- backend/src/main/java/org/code/backend/service/RoomService.java
+++ backend/src/main/java/org/code/backend/service/RoomService.java
@@ -1,4 +1,31 @@
 package org.code.backend.service;
 
-public class RoomService {
+import lombok.RequiredArgsConstructor;
+import org.code.backend.model.Room;
+import org.code.backend.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService implements IRoomService{
+    private final RoomRepository roomRepository;
+    @Override
+    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if (!photo.isEmpty()) {
+            byte[] photoBytes = photo.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
 }
