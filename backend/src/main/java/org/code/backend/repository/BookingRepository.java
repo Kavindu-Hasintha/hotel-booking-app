10c65855e30dde661aecef1168e9b277daa4f726
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-10 14:41:18 +0530

    Added all the functions in BookingForm.jsx

--- backend/src/main/java/org/code/backend/repository/BookingRepository.java
+++ backend/src/main/java/org/code/backend/repository/BookingRepository.java
@@ -4,7 +4,6 @@ import org.code.backend.model.BookedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<BookedRoom, Long>{
     List<BookedRoom> findByRoomId(Long roomId);
