761a4f1ba35f47e50dc072d3f229f6a5c21bb407
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-15 13:31:23 +0530

    Role model is added

--- backend/src/main/java/org/code/backend/model/Role.java
+++ backend/src/main/java/org/code/backend/model/Role.java
@@ -1,16 +1,18 @@
 package org.code.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
+/**
+ * @author Simpson Alfred
+ */
 @Entity
 @Getter
 @Setter
@@ -20,5 +22,33 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
     private Collection<User> users = new HashSet<>();
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public void assignRoleToUser(User user){
+        user.getRoles().add(this);
+        this.getUsers().add(user);
+    }
+
+    public void removeUserFromRole(User user){
+        user.getRoles().remove(this);
+        this.getUsers().remove(user);
+
+    }
+
+    public void removeAllUsersFromRole(){
+        if (this.getUsers() != null){
+            List<User> roleUsers = this.getUsers().stream().toList();
+            roleUsers.forEach(this :: removeUserFromRole);
+        }
+    }
+    public  String getName(){
+        return name != null? name : "";
+    }
 }
