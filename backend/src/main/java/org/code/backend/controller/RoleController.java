4285b7f8029f7be6f2a631a89688714372ebd640
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-16 16:31:01 +0530

    Create new role API is added

---
+++ backend/src/main/java/org/code/backend/controller/RoleController.java
@@ -0,0 +1,36 @@
+package org.code.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.code.backend.exception.RoleAlreadyExistException;
+import org.code.backend.model.Role;
+import org.code.backend.service.IRoleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.FOUND;
+
+@RestController
+@RequestMapping("/api/roles")
+@RequiredArgsConstructor
+public class RoleController {
+    private final IRoleService roleService;
+
+    @GetMapping("/all-roles")
+    public ResponseEntity<List<Role>> getAllRoles(){
+        return new ResponseEntity<>(roleService.getRoles(), FOUND);
+    }
+
+    @PostMapping("/create-new-role")
+    public ResponseEntity<String> createRole(@RequestBody Role theRole){
+        try{
+            roleService.createRole(theRole);
+            return ResponseEntity.ok("New role created successfully!");
+        }catch(RoleAlreadyExistException re){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(re.getMessage());
+
+        }
+    }
+}
