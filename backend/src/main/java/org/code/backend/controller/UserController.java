02f10bfdb0d2a1563096d9b372ad8c45f5bbce3e
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-17 16:33:22 +0530

    Registration, login api functions are added

--- backend/src/main/java/org/code/backend/controller/UserController.java
+++ backend/src/main/java/org/code/backend/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.code.backend.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.code.backend.model.User;
 import org.code.backend.service.IUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,31 +20,34 @@ public class UserController {
     private final IUserService userService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<User>> getUsers() {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<List<User>> getUsers(){
+
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
     }
 
     @GetMapping("/{email}")
-    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email) {
-        try {
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
+        try{
             User theUser = userService.getUser(email);
             return ResponseEntity.ok(theUser);
-        } catch (UsernameNotFoundException e) {
+        }catch (UsernameNotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
         }
     }
-
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email) {
-        try {
+    @PreAuthorize("hasRole('ROLE_ADMIN') or (hasRole('ROLE_USER') and #email == principal.username)")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
+        try{
             userService.deleteUser(email);
-            return ResponseEntity.ok("User deleted successfully.");
-        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.ok("User deleted successfully");
+        }catch (UsernameNotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting user.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting user: " + e.getMessage());
         }
     }
 }
