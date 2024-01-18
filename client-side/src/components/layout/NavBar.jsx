a2e8a00b79627927b945b72a7b06d2f365e600ee
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-18 15:56:33 +0530

    Registration and login part is completed

--- client-side/src/components/layout/NavBar.jsx
+++ client-side/src/components/layout/NavBar.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import Logout from "../auth/Logout";
 
 const NavBar = () => {
+  const [showAccount, setShowAccount] = useState(false);
+
+  const handleAccountClick = () => {
+    setShowAccount(!showAccount);
+  };
+
+  const isLoggedIn = localStorage.getItem("token");
+  const userRole = localStorage.getItem("userRole");
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
       <div className="container-fluid">
-        <Link to={"/"}>
-          <span className="hotel-color">Hotel Booking App</span>
+        <Link to={"/"} className="navbar-brand">
+          <span className="hotel-color">lakeSide Hotel</span>
         </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -20,59 +30,63 @@ const NavBar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarScroll">
           <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
             <li className="nav-item">
               <NavLink
                 className="nav-link"
                 aria-current="page"
-                to="/browse-all-rooms"
+                to={"/browse-all-rooms"}
               >
                 Browse all rooms
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to={"/admin"}>
-                Admin
-              </NavLink>
-            </li>
+
+            {isLoggedIn && userRole === "ROLE_ADMIN" && (
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to={"/admin"}>
+                  Admin
+                </NavLink>
+              </li>
+            )}
           </ul>
+
           <ul className="d-flex navbar-nav">
             <li className="nav-item">
               <NavLink className="nav-link" to={"/find-booking"}>
-                Find My Booking
+                Find my booking
               </NavLink>
             </li>
+
             <li className="nav-item dropdown">
               <a
-                className="nav-link dropdown-toggle"
+                className={`nav-link dropdown-toggle ${
+                  showAccount ? "show" : ""
+                }`}
                 href="#"
-                id="navbarDarkDropdownMenuLink"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
+                onClick={handleAccountClick}
               >
+                {" "}
                 Account
               </a>
+
               <ul
-                className="dropdown-menu dropdown-menu-dark"
-                aria-labelledby="navbarDarkDropdownMenuLink"
+                className={`dropdown-menu ${showAccount ? "show" : ""}`}
+                aria-labelledby="navbarDropdown"
               >
-                <li>
-                  <Link to={"/login"} className="dropdown-item">
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link to={"/profile"} className="dropdown-item">
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link to={"/logout"} className="dropdown-item">
-                    Logout
-                  </Link>
-                </li>
+                {isLoggedIn ? (
+                  <Logout />
+                ) : (
+                  <li>
+                    <Link className="dropdown-item" to={"/login"}>
+                      Login
+                    </Link>
+                  </li>
+                )}
               </ul>
             </li>
           </ul>
